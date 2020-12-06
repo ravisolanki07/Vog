5bb3691d7f1d5ea609b9df8edb1f71898f0ca740
Ravi_Solanki_CSharpNET <ravisolanki07@gmail.com>
2020-12-06 13:25:46 -0600

    v8.0 -- Implemented TestModule functionality

--- VogCodeChallenge.Console/Program.cs
+++ VogCodeChallenge.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
@@ -8,17 +9,66 @@ namespace VogCodeChallenge.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var list = QuestionClass.NamesList.Select(n =>
+            //var list = QuestionClass.NamesList.Select(n =>
+            //{
+            //    //iterate through list
+            //    return n + "temp";
+
+            //}).ToList();
+
+            ////just for displaying the data
+            //list.ForEach(n => { Console.WriteLine(n); });
+
+            Console.WriteLine(TESTModule(1));
+            Console.WriteLine(TESTModule(2));
+            Console.WriteLine(TESTModule("2.0f"));
+            Console.WriteLine(TESTModule("VogApp"));
+            Console.WriteLine(TESTModule(12.1212));
+            Console.WriteLine(TESTModule(0));
+            Console.ReadKey();
+        }
+
+        static object TESTModule(object input)
+        {
+            Dictionary<Type, int> typeDict = new Dictionary<Type, int>
             {
-                //iterate through list
-                return n + "temp";
+                {typeof(int),0},
+                {typeof(string),1},
+                {typeof(float),2}
+            };
+            var type = input.GetType();
 
-            }).ToList();
+            if (input.GetType() == typeof(int) || input.GetType() == typeof(float) || input.GetType() == typeof(string))
+            {
+                switch (typeDict[type])
+                {
+                    case 0:
+                        var inputInt = Convert.ToInt32(input);
+                        if (inputInt > 4)
+                        {
+                            return inputInt * 3;
+                        }
+                        if (inputInt < 1)
+                        {
+                            throw new InvalidOperationException("Please enter value which is greater than 0");
+                        }
+                        else
+                        {
+                            return inputInt * 2;
+                        }
+                    case 1:
+                        return input.ToString().ToUpper();
+                    case 2:
+                        var inputFloat = float.Parse(input.ToString());
+                        if (inputFloat == 1 || inputFloat == 2)
+                        {
+                            return 3.0f;
+                        }
+                        return input;
+                }
+            }
 
-            //just for displaying the data
-            list.ForEach(n => { Console.WriteLine(n); });
-
-            Console.ReadLine();
+            return input;
         }
     }
 }
