ce5628cddea406bee31bf33e6e42c8fb027f34cc
Ravi_Solanki_CSharpNET <ravisolanki07@gmail.com>
2020-12-06 12:49:32 -0600

    v7.0 -- Implemented a console app

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = QuestionClass.NamesList.Select(n =>
+            {
+                //iterate through list
+                return n + "temp";
+
+            }).ToList();
+
+            //just for displaying the data
+            list.ForEach(n => { Console.WriteLine(n); });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+}
