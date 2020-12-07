c03232004655d82caeebb0ac40b29669cf51969d
Ravi_Solanki_CSharpNET <ravisolanki07@gmail.com>
2020-12-07 12:03:40 -0600

    v9.0 -- Implemented AWS Lambda project

---
+++ VogCodeChallenge.AwsLambda/Function.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Text;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.Serialization.Json;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace VogCodeChallenge.AwsLambda
+{
+    public class Function
+    {
+        private static readonly JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records)
+            {
+                context.Logger.LogLine($"Event ID: {record.EventID}");
+                context.Logger.LogLine($"Event Name: {record.EventName}");
+
+                string streamRecordJson = SerializeObject(record.Dynamodb);
+                Console.WriteLine($"DynamoDB Record:");
+                Console.WriteLine(streamRecordJson);
+
+                var logEntry = new
+                {   
+                    Message = $"Item - '{record.Dynamodb.NewImage["Id"].N}' processed by lambda",
+                    DateTime = DateTime.Now
+                };
+
+                Console.WriteLine(logEntry);
+            }
+
+            context.Logger.LogLine("Stream processing complete.");
+        }
+
+        private string SerializeObject(object streamRecord)
+        {
+            using (var ms = new MemoryStream())
+            {
+                _jsonSerializer.Serialize(streamRecord, ms);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+}
